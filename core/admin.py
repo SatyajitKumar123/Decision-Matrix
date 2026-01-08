3003a1e534193208ce59968763a4d7a81e3d24c1
Satyajitkumar123 <satyajitkumar2472@gmail.com>
2026-01-08 08:42:48 +0000

    Feat: Configured Admin panel with calculated score display

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Task
 
-admin.site.register(Task)
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'urgency_score', 'risk_score', 'reward_score', 'get_total_score', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('title',)
+    
+    @admin.display(description='Total Score')
+    def get_total_score(self, obj):
+        return obj.total_score
